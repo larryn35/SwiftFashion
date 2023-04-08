9f7a6184f6ec0feeb4c19ed090bbefbe767db768
Larry Nguyen <larryn.dev@gmail.com>
2023-04-07 20:37:59 -0700

    Initial commit

---
+++ SwiftFashion/SwiftFashionApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftFashionApp.swift
+//  SwiftFashion
+//
+//  Created by Larry Nguyen on 4/7/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftFashionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
